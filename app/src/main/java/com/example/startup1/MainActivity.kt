7c7d7de44076fa102b97b01b77af7792bee80130
AllanTumu <allantumu1962@gmail.com>
2019-07-09 16:27:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/startup1/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.startup1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    //Method 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /*buFindMyAge.setOnClickListener{
+            //Runs when button is clicked
+            val yearOfBirth = YOB.text.toString().toInt()
+            val curYear = Calendar.getInstance().get(Calendar.YEAR)
+            val myAge = curYear - yearOfBirth
+            output.text = "You are $myAge years old"
+        }*/
+    }
+
+    //Method 2
+    fun click(view:View){
+        val yearOfBirth = YOB.text.toString().toInt()
+        val curYear = Calendar.getInstance().get(Calendar.YEAR)
+        val myAge = curYear - yearOfBirth
+        output.text = "You are $myAge years old"
+
+    }
+
+}
+
+
